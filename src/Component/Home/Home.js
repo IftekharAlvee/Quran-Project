c1bb27ec400a97737b4a50543a553e797ed3bc2d
Iftekhar Alvee <alveee1998@gmail.com>
2021-03-12 00:57:00 +0600

    Bismillah

---
+++ src/Component/Home/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
+
+const Home = () => {
+    return (
+        <div>
+            <h1>This is home</h1>
+        </div>
+    );
+};
+
+export default Home;
